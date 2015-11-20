a01e74008c0fe0927c63ac0791f4bb1ce11af4c7
Niko G <guoxiangliu@gmail.com>
2015-11-20 09:23:03 +0800

    Update updatednsmasqconf.sh

--- files/usr/bin/updatednsmasqconf.sh
+++ files/usr/bin/updatednsmasqconf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python /usr/bin/gfwlist2dnsmasq.py 
-echo "$(date): update dnsmasq config ...">>/tmp/log/dnsmasq-config-update.log
+echo "$(date): update dnsmasq gfwlist ...">>/tmp/log/dnsmasq-gfwlist-update.log
