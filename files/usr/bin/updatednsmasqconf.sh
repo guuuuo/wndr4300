3317cee11134c2bb6b426165f2e8610c0d116ca4
Niko G <guoxiangliu@gmail.com>
2015-11-20 09:24:41 +0800

    Update updatednsmasqconf.sh

--- files/usr/bin/updatednsmasqconf.sh
+++ files/usr/bin/updatednsmasqconf.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-python /usr/bin/gfwlist2dnsmasq.py 
-echo "$(date): update dnsmasq gfwlist ...">>/tmp/log/dnsmasq-config-update.log
+
+adblock() {
+        wget -4 --no-check-certificate -O - https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt |
+        grep ^\|\|[^\*/]*\^$ |
+        sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' | uniq > /etc/dnsmasq.d/adblock.conf
+
+        wget -4 --no-check-certificate -O - https://raw.githubusercontent.com/kcschan/AdditionalAdblock/master/list.txt |
+        grep ^\|\|[^\*/]*\^$ |
+        sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' >> /etc/dnsmasq.d/adblock.conf
+}
+
+gfwlist() {
+        python /usr/bin/gfwlist2dnsmasq.py
+        echo "$(date): update dnsmasq gfwlist...">>/tmp/log/dnsmasq-config-update.log
+}
+
+#adblock
+gfwlist
+
+/etc/init.d/dnsmasq restart
